81670cd2bf837ca3207ff968e731185a94bcbb26
Athsoatle <zaharucheba@gmail.com>
2020-11-02 02:06:05 +0300

    Initial commit

---
+++ src/main/java/com/prachtig/PrachtigApplication.java
@@ -0,0 +1,13 @@
+package com.prachtig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrachtigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrachtigApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/prachtig/PrachtigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prachtig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrachtigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
